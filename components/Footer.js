4b7c39d92331683c72eb53ee38a8f0f85c5bbd62
Rebecca Combs <71912974+rebeccacombs@users.noreply.github.com>
2022-07-10 15:56:12 -0400

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react' 
 import {AiTwotoneHeart} from "react-icons/ai"; 
+import Link from "next/link"; 
 
 function Footer() {
     return (
         <footer className="w-full h-16 bg-stone-200 flex justify-center items-center">
-            <h1 className="text-xl text-teal-900 mr-2">© 2022 made by <a href="/" className="cursor-pointer font-semibold text-teal-900 hover:text-black">CICD</a></h1>
+            <h1 className="text-xl text-teal-900 mr-2">© 2022 made by <Link href="/"><a className="cursor-pointer font-semibold text-teal-900 hover:text-black">CICD</a></Link></h1>
             <AiTwotoneHeart className="text-black"/>
             <h1 className="text-xl text-teal-900 ml-2">All rights reserved.</h1>
         </footer>
