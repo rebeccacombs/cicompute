c4c4adecf007ffcbfd091b4bc71a6e6645d786c1
Rebecca Combs <71912974+rebeccacombs@users.noreply.github.com>
2022-07-16 11:36:43 -0400

    footer padding

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 function Footer() {
     return (
-        <footer className="w-full h-16 bg-stone-200 flex justify-center items-center mt-36">
+        <footer className="w-full h-16 bg-stone-200 flex justify-center items-center mt-36 p-8">
             <h1 className="text-xl text-teal-900 mr-2">© 2022 made by <Link href="/"><a className="cursor-pointer font-semibold text-teal-900 hover:text-black">CICD</a></Link></h1>
             <AiTwotoneHeart className="text-black"/>
             <h1 className="text-xl text-teal-900 ml-2">All rights reserved.</h1>
